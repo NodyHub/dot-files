94b346b97f71b84bfbe8564e288a50984bb31e64
NodyHub <jan.harrie@hashicorp.com>
2022-07-15 15:56:08 +0200

    changed temp dir

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,11 +1,10 @@
- #!/bin/bash
- 
- cd /tmp
- git clone https://github.com/NodyHub/dot-files.git
- cd /tmp/dot-files
- ./deploy.sh
- cd /tmp
- rm -rf /tmp/dot-files
- sudo chsh -s $(which zsh) $(whoami)
- 
- exit 0
+#!/bin/bash
+
+git clone https://github.com/NodyHub/dot-files.git /tmp/dot-files
+cd /tmp/dot-files
+./deploy.sh
+cd -
+rm -rf /tmp/dot-files
+sudo chsh -s $(which zsh) $(whoami)
+
+exit 0
