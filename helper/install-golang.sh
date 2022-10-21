8cc8341eac16ebc9f7d8cf624e12e341a5903293
NodyHub <jan.harrie@hashicorp.com>
2022-10-21 11:41:02 +0200

    install gopls also

--- helper/install-golang.sh
+++ helper/install-golang.sh
@@ -8,4 +8,9 @@ ln -s /usr/local/go/bin/go /usr/local/bin/go
 ln -s /usr/local/go/bin/gofmt /usr/local/bin/gofmt
 rm /tmp/$ARCHIVE
 
+echo ">> Install gopls"
+GOPATH=/tmp/go go install golang.org/x/tools/gopls@latest
+cp /tmp/go/bin/gopls /usr/local/go/bin/gopls
+ln -s /usr/local/go/bin/gopls /usr/local/bin/gopls
+
 exit 0
