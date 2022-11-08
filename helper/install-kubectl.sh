b86ff861f5a54be0de200fc1e380d0fa1beb2869
NodyHub <jan.harrie@hashicorp.com>
2022-11-08 10:23:00 +0100

    removed sudo

--- helper/install-kubectl.sh
+++ helper/install-kubectl.sh
@@ -3,7 +3,7 @@
 echo ">> Install kubectl"
 wget -q -O /tmp/stable.txt https://dl.k8s.io/release/stable.txt
 wget -q -O /tmp/kubectl "https://dl.k8s.io/release/$(cat /tmp/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 /tmp/kubectl /usr/local/bin/kubectl
+install -o root -g root -m 0755 /tmp/kubectl /usr/local/bin/kubectl
 rm /tmp/kubectl
 rm /tmp/stable.txt
 
--- helper/install-minikube.sh
+++ helper/install-minikube.sh
@@ -2,7 +2,7 @@
 
 echo ">> Install Minikube"
 wget -q -O /tmp/minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install /tmp/minikube /usr/local/bin/minikube
+install /tmp/minikube /usr/local/bin/minikube
 rm /tmp/minikube
 
 exit 0
